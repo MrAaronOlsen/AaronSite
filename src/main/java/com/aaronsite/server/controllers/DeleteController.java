3f5a1766d73f4436711723eebcfb98288d100538
Aaron Olsen <mraaronolsen@gmail.com>
2019-11-12 19:34:36 -0700

    Multipl cors origins

--- src/main/java/com/aaronsite/server/controllers/DeleteController.java
+++ src/main/java/com/aaronsite/server/controllers/DeleteController.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 import static com.aaronsite.server.controllers.MasterController.BASE_URL;
 
-@CrossOrigin(origins = "https://aaron-site.herokuapp.com, http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "https://aaron-site.herokuapp.com"})
 @RestController
 @RequestMapping(BASE_URL)
 public class DeleteController extends MasterController {
